2be2382eb4eb01ca475b79ce4e3742a9d49119ea
akhad97 <ahadjon.abdullaev1997@gmail.com>
2023-12-05 11:08:20 +0500

    bug in settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -254,14 +254,16 @@ CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    # "https://api.parvoz.site.uz",
-    "https://expressparvoz.uz",
-    "http://45.129.170.136",
-    'http://localhost:5173',
-    'http://localhost:3000',
-    "http://127.0.0.1:8000",
-]
+CORS_ALLOW_ALL_ORIGINS = True
+
+# CORS_ALLOWED_ORIGINS = [
+#     # "https://api.parvoz.site.uz",
+#     "https://expressparvoz.uz",
+#     "http://45.129.170.136",
+#     'http://localhost:5173',
+#     'http://localhost:3000',
+#     "http://127.0.0.1:8000",
+# ]
 
 
 OWNER = "owner"
