091b1e2fa8baad14e1edef3d8e3e4e309332a2c6
akhad97 <ahadjon.abdullaev1997@gmail.com>
2023-12-05 13:55:28 +0500

    fucking bug fixed

--- main/apps/client/views.py
+++ main/apps/client/views.py
@@ -192,6 +192,8 @@ visa_client_delete_api_view = VisaClientDeleteAPIView.as_view()
 
 
 class ClientPDFView(APIView):
+    agent_id=None
+    image=None
     def post(self, request, *args, **kwargs):
         global image, agent_id, select, select_1, select_2, select_3, select_4, select_5, select_6, select_7, select_8
         image = request.data.get('image')
@@ -254,15 +256,15 @@ class ClientPDFView(APIView):
                                             'tourpackage_end_day': tourpackage_end_day,
                                             'agent_id': agent_id,
                                             'image': image,
-                                            'select': select,
-                                            'select_1': select_1,
-                                            'select_2': select_2,
-                                            'select_3': select_3,
-                                            'select_4': select_4,
-                                            'select_5': select_5,
-                                            'select_6': select_6,
-                                            'select_7': select_7,
-                                            'select_8': select_8
+                                            # 'select': select,
+                                            # 'select_1': select_1,
+                                            # 'select_2': select_2,
+                                            # 'select_3': select_3,
+                                            # 'select_4': select_4,
+                                            # 'select_5': select_5,
+                                            # 'select_6': select_6,
+                                            # 'select_7': select_7,
+                                            # 'select_8': select_8
                                         })
         pdf_file = pdfkit.from_string(html_content, False) 
         response = HttpResponse(pdf_file, content_type='application/pdf') 
