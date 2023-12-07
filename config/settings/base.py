ff1fc9b57fa92b105cafd261f6f6ac313af8e566
akhad97 <ahadjon.abdullaev1997@gmail.com>
2023-12-07 16:11:38 +0500

    pdf generating bug fixed

--- config/settings/base.py
+++ config/settings/base.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 from datetime import timedelta
 from pathlib import Path
+import locale
+from django.utils.translation import ugettext_lazy as _
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
@@ -214,7 +216,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOCALE = 'uz_UZ'
+locale.setlocale(locale.LC_TIME, LOCALE)
 
+USE_L10N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
