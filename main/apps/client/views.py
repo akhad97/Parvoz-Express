18bbf14da132e065209622b6644b539b6eab8c88
akhad97 <ahadjon.abdullaev1997@gmail.com>
2023-12-07 13:35:03 +0500

    pdf generating bug fixed

--- main/apps/client/views.py
+++ main/apps/client/views.py
@@ -269,8 +269,9 @@ class ClientPDFView(APIView):
         tourpackage_end_month = client.tour_package.end_date.strftime('%B')
         tourpackage_end_day = client.tour_package.end_date.day
 
-        BASE_URL = 'https://api.parvoz.site.uz/media/'
-        complete_signin_image_url = urljoin(BASE_URL, str(client_signin_image))
+        # BASE_URL = ' https://bb1b-84-54-74-20.ngrok-free.app/media/'
+        BASE_URL = ' https://api.parvoz.site.uz/media/'
+        complete_signin_image_url = urljoin(BASE_URL, str(client_signin_image.url))
         year =  datetime.now().year
         month =  datetime.now().strftime('%B')
         day =  datetime.now().day
